bd4bf60f6daef2730b247918dbfb8230166817cd
Steinbergmann <trestrebien@gmail.com>
2020-10-03 15:31:59 +0300

    ver. 0.1

---
+++ Assets/Scripts/Scene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene : MonoBehaviour
+{
+
+    public void Restartlevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+   
+}
